06255d020f5a1532c012f95e6e6d601a27e8e92a
Shishir Karkera <dn.shishirgk@gmail.com>
2026-01-25 12:48:00 +0530

    feat: initialize core server with database, socket, and receipt listener.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import os from 'os'
 import http from 'http'
 import app from './app'
 import prisma from './prisma/client'
@@ -17,9 +18,26 @@ async function start() {
 
     const server = http.createServer(app)
     initSocket(server)
-    server.listen(port, () => {
+    server.listen(port, '0.0.0.0', () => {
+      const interfaces = os.networkInterfaces()
+      const addresses: string[] = []
+      for (const k in interfaces) {
+        for (const k2 in interfaces[k]!) {
+          const address = interfaces[k]![k2]
+          if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address)
+          }
+        }
+      }
+
       // eslint-disable-next-line no-console
       console.log(`API server listening on port ${port}`)
+      // eslint-disable-next-line no-console
+      console.log(`➜  Local:   http://localhost:${port}/`)
+      for (const address of addresses) {
+        // eslint-disable-next-line no-console
+        console.log(`➜  Network: http://${address}:${port}/`)
+      }
     })
   } catch (error) {
     // eslint-disable-next-line no-console
